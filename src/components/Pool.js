e9d3d2209784691c9a0360b100cedc715539035f
Louis Heal <louisheal@live.co.uk>
2024-05-21 22:46:35 +0100

    create pool view template

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -1,9 +1,39 @@
 import '../styles/Pool.css';
 
 const Pool = (props) => {
-  return <>
-    <h2>{props.name}</h2>
-  </>
+  return <div className='pool flex align-items-center'>
+
+    <div>
+    ICONS
+    </div>
+
+    <div>
+      {props.pool_name.split(' ').map((word, index) => (
+        <h4 key={index}>{word}</h4>
+      ))}
+    </div>
+
+    <div className='flex'>
+      <div className='align-left'>
+        <p>Borrowing Interest ({props.token_one_symbol}):</p>
+        <p>Borrowing Interest ({props.token_two_symbol}):</p>
+        <p>Trading Fees (7 Day Avg.):</p>
+      </div>
+
+      <div className='align-right'>
+        <p>{props.borrow_rate_one.toFixed(3)}%</p>
+        <p>{props.borrow_rate_two.toFixed(3)}%</p>
+        <p>{props.trading_fee.toFixed(3)}%</p>
+      </div>
+    </div>
+
+    <div>
+      <button>
+        <h3>Simulate</h3>
+      </button>
+    </div>
+
+  </div>
 };
 
 export default Pool;
