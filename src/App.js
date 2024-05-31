1205dc0daae7f67753cfaf67a28992abd45c3d8a
Louis Heal <louisheal@live.co.uk>
2024-05-31 12:23:20 +0100

    add loading screen before pool data is loaded

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const api = axios.create({
 class App extends Component {
 
   state = {
-    pools: [],
+    pools: null,
     charts: {},
   };
 
@@ -52,16 +52,27 @@ class App extends Component {
             </h1>
           </div>
           <div className='pool-container'>
-          {pools.map(pool =>
-            <Pool key={pool.pool_id}
-                  pool_id={pool.pool_id}
-                  pool_name={pool.pool_name}
-                  borrow_rate_one={pool.borrow_rate_one}
-                  borrow_rate_two={pool.borrow_rate_two}
-                  token_one_symbol={pool.token_one_symbol}
-                  token_two_symbol={pool.token_two_symbol}
-                  trading_fee={pool.trading_fee}
-                  chart_data={charts[pool.pool_id]}/>)}
+          {pools ? (
+            pools.map(pool =>
+              <Pool key={pool.pool_id}
+                pool_id={pool.pool_id}
+                pool_name={pool.pool_name}
+                borrow_rate_one={pool.borrow_rate_one}
+                borrow_rate_two={pool.borrow_rate_two}
+                token_one_symbol={pool.token_one_symbol}
+                token_two_symbol={pool.token_two_symbol}
+                trading_fee={pool.trading_fee}
+                chart_data={charts[pool.pool_id]}/>)
+          ) : (
+            <div>
+              <h1>
+                Loading...
+              </h1>
+              <p>
+                (Please wait up to 5 minutes for the pools to load.)
+              </p>
+            </div>
+          )}
           </div>
           <div className='footer'>
           
